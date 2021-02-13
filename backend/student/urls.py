1a2125437f6eadce6023cfe51035c584292b62b8
DarshitJain04 <jain.38@iitj.ac.in>
2021-02-14 00:20:25 +0530

    corrected flake8 errors

--- backend/student/urls.py
+++ backend/student/urls.py
@@ -10,4 +10,4 @@ urlpatterns = [
     path('get_resumes/', views.getResumes.as_view(), name="student-resumes"),
     path('add_resume/', views.addResume.as_view(), name="add-student-resume"),
     path('delete_resume/<int:pk>', views.deleteResume.as_view(), name="delete-student-resume"),
-] 
+]
--- backend/student/views.py
+++ backend/student/views.py
@@ -1,12 +1,10 @@
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import StudentProfileSerializer, ResumeSerializer
 from .models import StudentProfile, Resume
 from rest_framework.views import APIView
-from rest_framework import viewsets
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
-from rest_framework.parsers import FileUploadParser, MultiPartParser, FormParser
+from rest_framework.parsers import FileUploadParser
 
 
 class addStudentDetails(APIView):
@@ -20,7 +18,7 @@ class addStudentDetails(APIView):
         user.first_name = data['user']['first_name']
         user.last_name = data['user']['last_name']
         user.save()
-        profile = StudentProfile.objects.create(user = user, **data)
+        profile = StudentProfile.objects.create(user=user, **data)
         profile.save()
         return Response(StudentProfileSerializer(profile).data, status=status.HTTP_200_OK)
 
@@ -32,7 +30,7 @@ class getStudentDetails(APIView):
         user = request.user
         student_profile = StudentProfile.objects.filter(user=user)
         serializer = StudentProfileSerializer(student_profile, many=True)
-        return Response(serializer.data, status = status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class updateStudentDetails(APIView):
@@ -49,9 +47,8 @@ class updateStudentDetails(APIView):
         user.save()
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status = status.HTTP_200_OK)
-        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
-
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class deleteStudentDetails(APIView):
@@ -59,31 +56,30 @@ class deleteStudentDetails(APIView):
 
     def delete(self, request, *args, **kwargs):
         user = request.user
-        profile = StudentProfile.objects.get(user = user)
-        operation  = profile.delete()
+        profile = StudentProfile.objects.get(user=user)
+        operation = profile.delete()
         data = {}
         if operation:
             data["operation status"] = "Successfully deleted user profile"
         else:
             data["operation status"] = "Failed to delete user profile"
-        return Response(data = data)
+        return Response(data=data)
 
 
 class getResumes(APIView):
     permission_classes = (IsAuthenticated,)
 
     def get(self, request, *args, **kwargs):
-        resumes = Resume.objects.filter(student__user = request.user)
+        resumes = Resume.objects.filter(student__user=request.user)
         serializer = ResumeSerializer(resumes, many=True)
-        return Response(serializer.data, status = status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class addResume(APIView):
     permission_classes = (IsAuthenticated,)
     parser_classes = (FileUploadParser,)
 
-    def post(self, request, format = None):
-        user = request.user
+    def post(self, request, format=None):
         file = request.data['file']
         serializer = ResumeSerializer(file=file, data=request.data)
         if serializer.is_valid():
@@ -97,8 +93,7 @@ class deleteResume(APIView):
     permission_classes = (IsAuthenticated,)
 
     def delete(self, request, pk):
-        user = request.user
-        resume = Resume.objects.get(id = pk)
+        resume = Resume.objects.get(id=pk)
         data = {}
         if(resume.is_verified):
             data["operation status"] = "Can not delete verified resume"
@@ -108,4 +103,4 @@ class deleteResume(APIView):
                 data["operation status"] = "Successfully deleted resume"
             else:
                 data["operation status"] = "Failed to delete resume"
-        return Response(data = data)
+        return Response(data=data)
