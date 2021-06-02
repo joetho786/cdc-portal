14ce8297dac1e01f57a6f00a933d3eee5382f5ba
manav1403 <manavavni@gmail.com>
2021-06-02 19:22:05 +0530

    proxy_to_backend

---
+++ frontend/src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+    target: `http://${process.env.BACKEND_HOST ? process.env.BACKEND_HOST : '127.0.0.1'}:8000`,
+      changeOrigin: true,
+    })
+  );
+};
