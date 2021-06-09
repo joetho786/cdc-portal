05a2d2e82aa08f611fd609f8f92b2eb69e15231a
Mohan Chhabaria <63086398+MohanChhabaria@users.noreply.github.com>
2021-06-09 13:08:54 +0530

    aipc norms

--- frontend/src/pages/AIPCNorms.js
+++ frontend/src/pages/AIPCNorms.js
@@ -5,6 +5,7 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import { Container, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import Footer from '../../src/components/Footer/Footer';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -87,6 +88,7 @@ const AIPCNorms = () => {
               </Grid>
             </Grid>
           </Container>
+          <Footer />
         </>
       )}
     </div>
