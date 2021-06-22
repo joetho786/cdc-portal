2a10b7366a86f72af45ff6b4b7c927a8b8909173
Mohan Chhabaria <63086398+MohanChhabaria@users.noreply.github.com>
2021-06-22 18:02:42 +0530

    minor changes

--- frontend/src/pages/ChairmanMessage.js
+++ frontend/src/pages/ChairmanMessage.js
@@ -54,7 +54,7 @@ const ChairmanMessage = () => {
   };
 
   return (
-    <div style={{ height: '100vh', width: '100%' }}>
+    <div style={{ height: 'auto', width: '100%' }}>
       {loading ? (
         <Loading />
       ) : (
