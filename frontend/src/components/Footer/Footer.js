3188de4654d3cec92be89c6e09f4fbba2d049aca
manav1403 <manavavni@gmail.com>
2021-07-22 10:56:41 +0530

    changed footer

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -11,7 +11,7 @@ import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import GitHubIcon from '@material-ui/icons/GitHub';
 function Copyright() {
   return (
-    <Typography variant="body2" style={{ color: 'white' }} align="center">
+    <Typography variant="body2" style={{ color: '#012970' }} align="center">
       {'Copyright © '}
       <Link color="inherit">CDC, IITJ</Link> {new Date().getFullYear()}
       {'.'}
@@ -47,7 +47,10 @@ const footers = [
 
 const useStyles = makeStyles((theme) => ({
   footer: {
-    backgroundColor: '#212121',
+    backgroundColor: 'white',
+    background:
+      'url(https://www.linkpicture.com/q/hero-bg_1.png) top center no-repeat',
+    color: '#012970',
     padding: theme.spacing(3, 0),
   },
   '@global': {
@@ -63,140 +66,130 @@ export default function Footer(props) {
   const classes = useStyles();
 
   return (
-    <footer className={classes.footer}>
-      <Container justify="space-evenly" maxWidth="lg">
-        <Container
-          justify="space-evenly"
-          maxWidth="lg"
-          component="footer"
-          className={classes.footer}
-        >
-          <Grid container spacing={4}>
-            <Grid item xs={6} sm={3}>
+    <>
+      <footer className={classes.footer}>
+        <Grid container style={{ margin: '0 5%' }}>
+          <Grid item xs={6} sm={3}>
+            <Typography
+              component={'span'}
+              variant="h6"
+              align="center"
+              gutterBottom
+            >
+              <ul align="left">
+                <li>
+                  <Typography style={{ color: '#012970' }} variant="h6">
+                    <b>CDC IITJ</b>
+                  </Typography>
+                </li>
+                <li>
+                  <Typography variant="subtitle1" style={{ color: '#012970' }}>
+                    Indian Institute of Technology Jodhpur
+                  </Typography>
+                  <Typography variant="subtitle1" style={{ color: '#012970' }}>
+                    NH 65 Nagaur Road
+                  </Typography>
+                  <Typography variant="subtitle1" style={{ color: '#012970' }}>
+                    Karwar 342037, Jodhpur District
+                  </Typography>
+                </li>
+                <li>
+                  <Typography variant="subtitle1" style={{ color: '#012970' }}>
+                    <b>Phone:</b> +91 291 2801154
+                  </Typography>
+                  <Typography variant="subtitle1" style={{ color: '#012970' }}>
+                    <b>Phone:</b> +91 291 2801153
+                  </Typography>
+                  <Typography variant="subtitle1" style={{ color: '#012970' }}>
+                    <b>Email:</b>{' '}
+                    <a
+                      href="mailto:placement@iitj.ac.in"
+                      style={{ textDecoration: 'none', color: '#012970' }}
+                    >
+                      placement@iitj.ac.in
+                    </a>
+                  </Typography>
+                </li>
+              </ul>
+            </Typography>
+          </Grid>
+          {footers.map((footer) => (
+            <Grid item xs={6} sm={3} key={footer.title}>
               <Typography
                 component={'span'}
+                style={{ color: '#012970' }}
                 variant="h6"
-                align="center"
                 gutterBottom
               >
-                <ul align="left">
-                  <li>
-                    <Typography style={{ color: 'white' }} variant="h6">
-                      <b>CDC IITJ</b>
-                    </Typography>
-                  </li>
-                  <li>
-                    <Typography variant="subtitle1" style={{ color: 'white' }}>
-                      Indian Institute of Technology Jodhpur
-                    </Typography>
-                    <Typography variant="subtitle1" style={{ color: 'white' }}>
-                      NH 65 Nagaur Road
-                    </Typography>
-                    <Typography variant="subtitle1" style={{ color: 'white' }}>
-                      Karwar 342037, Jodhpur District
-                    </Typography>
-                  </li>
-                  <li>
-                    <Typography variant="subtitle1" style={{ color: 'white' }}>
-                      <b>Phone:</b> +91 291 2801154
-                    </Typography>
-                    <Typography variant="subtitle1" style={{ color: 'white' }}>
-                      <b>Phone:</b> +91 291 2801153
-                    </Typography>
-                    <Typography variant="subtitle1" style={{ color: 'white' }}>
-                      <b>Email:</b>{' '}
-                      <a
-                        href="mailto:placement@iitj.ac.in"
-                        style={{ textDecoration: 'none', color: 'white' }}
-                      >
-                        placement@iitj.ac.in
-                      </a>
-                    </Typography>
-                  </li>
-                </ul>
+                <b>{footer.title}</b>
               </Typography>
+              <ul>
+                {footer.description.map((item) => (
+                  <li key={item}>
+                    <Link
+                      href={item[1]}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      variant="subtitle1"
+                      style={{ color: '#012970' }}
+                    >
+                      {item[0]}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
             </Grid>
-            {footers.map((footer) => (
-              <Grid item xs={6} sm={3} key={footer.title}>
-                <Typography
-                  component={'span'}
-                  style={{ color: 'white' }}
-                  variant="h6"
-                  gutterBottom
-                >
-                  <b>{footer.title}</b>
-                </Typography>
-                <ul>
-                  {footer.description.map((item) => (
-                    <li key={item}>
-                      <Link
-                        href={item[1]}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        variant="subtitle1"
-                        style={{ color: 'white' }}
-                      >
-                        {item[0]}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </Grid>
-            ))}
-          </Grid>
-          <Typography
-            component={'span'}
-            variant="h6"
-            align="center"
-            gutterBottom
-          >
-            <div>
-              <Tooltip arrow title="facebook">
-                <IconButton
-                  href="https://www.facebook.com/SPC.IITJ/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  variant="outlined"
-                  style={{ color: 'white' }}
-                >
-                  <FacebookIcon />
-                </IconButton>
-              </Tooltip>
-              <Tooltip arrow title="Linkedin">
-                <IconButton
-                  href="https://in.linkedin.com/in/career-development-cell-iit-jodhpur-62a31352"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  style={{ color: 'white' }}
-                  variant="outlined"
-                >
-                  <LinkedInIcon style={{ color: 'white' }} />
-                </IconButton>
-              </Tooltip>
-              <Tooltip arrow title="GitHub">
-                <IconButton
-                  href="https://github.com/devlup-labs/cdc-portal"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  variant="outlined"
-                  style={{ color: 'white' }}
-                >
-                  <GitHubIcon />
-                </IconButton>
-              </Tooltip>
-            </div>
-          </Typography>
-          <Typography
-            variant="subtitle1"
-            align="center"
-            style={{ color: 'white' }}
-            component="p"
-          >
-            Designed & Maintained by Career Development Cell, IIT Jodhpur
-          </Typography>
-          <Copyright />
-        </Container>
+          ))}
+        </Grid>
+      </footer>
+      <Container style={{ background: '#f6f9ff', padding: '20px 0' }}>
+        <Typography component={'span'} variant="h6" align="center" gutterBottom>
+          <div>
+            <Tooltip arrow title="facebook">
+              <IconButton
+                href="https://www.facebook.com/SPC.IITJ/"
+                target="_blank"
+                rel="noopener noreferrer"
+                variant="outlined"
+                style={{ color: '#012970' }}
+              >
+                <FacebookIcon />
+              </IconButton>
+            </Tooltip>
+            <Tooltip arrow title="Linkedin">
+              <IconButton
+                href="https://in.linkedin.com/in/career-development-cell-iit-jodhpur-62a31352"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ color: '#012970' }}
+                variant="outlined"
+              >
+                <LinkedInIcon style={{ color: '#012970' }} />
+              </IconButton>
+            </Tooltip>
+            <Tooltip arrow title="GitHub">
+              <IconButton
+                href="https://github.com/devlup-labs/cdc-portal"
+                target="_blank"
+                rel="noopener noreferrer"
+                variant="outlined"
+                style={{ color: '#012970' }}
+              >
+                <GitHubIcon />
+              </IconButton>
+            </Tooltip>
+          </div>
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          style={{ color: '#012970' }}
+          component="p"
+        >
+          Designed & Maintained by Career Development Cell, IIT Jodhpur
+        </Typography>
+        <Copyright />
       </Container>
-    </footer>
+    </>
   );
 }
