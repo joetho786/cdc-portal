b9953ac1794c7b4a43432aa5decd888b214368b0
DarshitJain04 <jain.38@iitj.ac.in>
2021-07-24 18:04:41 +0530

    changed UI of Programs page and added Accordion

--- frontend/src/pages/Programs.js
+++ frontend/src/pages/Programs.js
@@ -2,63 +2,28 @@ import React from 'react';
 import Loading from '../components/Loading';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import { Container, Typography } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import Link from '@material-ui/core/Link';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableRow from '@material-ui/core/TableRow';
-import Table from '@material-ui/core/Table';
+import { Container } from '@material-ui/core';
 import FadeInWhenVisible from '../components/Animation/FadeIn';
-import FadeUpBigDataWhenVisible from '../components/Animation/FadeUpBigData';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    display: 'flex',
-    marginBottom: '2rem',
-    [theme.breakpoints.down(460)]: {
-      padding: 15,
-    },
-  },
-  paper: {
-    padding: theme.spacing(2),
-    [theme.breakpoints.up(460)]: {
-      paddingInline: 40,
-    },
-    width: 'auto',
-    color: 'rgb(0,0,0)',
-    fontSize: '1rem',
-  },
-  text: {
-    color: 'rgb(0,0,0)',
-    fontsize: '1rem',
-    textAlign: 'center',
-  },
-  table: {
-    minWidth: 'auto',
-    maxWidth: 'auto',
-    border: 'black',
-  },
-  tablewrapper: {
-    display: 'flex',
-    marginTop: 10,
-    justifyContent: 'left',
-  },
-  heads: {
-    fontSize: '1rem',
-    fontWeight: 'bold',
-    color: 'rgb(0,0,0)',
-    textAlign: 'center',
-  },
-}));
+import ProgramCard from '../components/ProgramCard';
+import styles from '../styles/pages/Programs.module.css';
 
 function createData(name, link) {
   return { name, link };
 }
 
-const btechrows = [
+const Programs = [
+  'Bachelor of Technology Programs',
+  'Master of Technology Programs',
+  'Master of Science Programs',
+  'Master of Science - Master of Technology Programs',
+  'Master of Technology - Doctor of Philosophy Dual Degree Programs',
+  'Doctor of Philosophy Programs (Core Sciences)',
+  'Doctor of Philosophy Programs (Engineering Sciences)',
+  'Doctor of Philosophy Programs (Inter Disciplinary Areas)',
+  'Doctor of Philosophy Programs (Humanities and Social Sciences)',
+];
+
+const bTechProgram = [
   createData(
     'B. Tech. (Bioengineering)',
     'http://iitj.ac.in/academics/index.php?id=acad_program&&prog=12'
@@ -93,7 +58,7 @@ const btechrows = [
   ),
 ];
 
-const mtechrows = [
+const mTechProgram = [
   createData(
     'M. Tech. (Bioscience and Bioengineering)',
     'http://iitj.ac.in/academics/index.php?id=acad_program&&prog=19'
@@ -132,7 +97,7 @@ const mtechrows = [
   ),
 ];
 
-const mscrows = [
+const mScProgram = [
   createData(
     'M.Sc. (Chemistry)',
     'http://iitj.ac.in/academics/index.php?id=acad_program&&prog=16'
@@ -147,14 +112,14 @@ const mscrows = [
   ),
 ];
 
-const mscmtechrows = [
+const mScMtechProgram = [
   createData(
     'Mathematics & Data Science',
     'http://iitj.ac.in/academics/index.php?id=acad_program&&prog=34'
   ),
 ];
 
-const mtechphdrows = [
+const mTechPhdProgram = [
   createData(
     'M.Tech.-Ph.D. Dual Degree (Bioscience & Bioengineering)',
     'http://iitj.ac.in/academics/index.php?id=acad_program&&prog=10'
@@ -201,7 +166,7 @@ const mtechphdrows = [
   ),
 ];
 
-const phdcorerows = [
+const phdCore = [
   createData(
     'Ph.D. (Chemistry)',
     'http://iitj.ac.in/academics/index.php?id=curriculum&prog=phd&dep=chemistry'
@@ -216,7 +181,7 @@ const phdcorerows = [
   ),
 ];
 
-const phdesrows = [
+const phdEngineeringSciences = [
   createData(
     'Ph.D. (Biosciences and Bioengineering)',
     'http://iitj.ac.in/academics/index.php?id=curriculum&prog=phd&dep=biology'
@@ -236,7 +201,7 @@ const phdesrows = [
   ),
 ];
 
-const phdintrows = [
+const phdInterDisciplinary = [
   createData(
     'Ph.D. (AUV Technologies)',
     'http://iitj.ac.in/academics/index.php?id=acad_program&&prog=phd'
@@ -267,7 +232,7 @@ const phdintrows = [
   ),
 ];
 
-const phdhumrows = [
+const phdHumanities = [
   createData(
     'Ph.D. (Humanities & Social Sciences)',
     'http://iitj.ac.in/academics/index.php?id=curriculum&prog=phd&dep=hss'
@@ -275,7 +240,6 @@ const phdhumrows = [
 ];
 
 const Programmes = () => {
-  const classes = useStyles();
   const loading = false;
 
   return (
@@ -284,263 +248,59 @@ const Programmes = () => {
         <Loading />
       ) : (
         <>
-          <Container maxWidth="lg" className={classes.root}>
-            <Grid container spacing={3}>
-              <Grid style={{ marginTop: '30px' }} item xs={12}>
-                <FadeInWhenVisible>
-                  <Paper className={classes.paper}>
-                    <Typography
-                      component="h5"
-                      variant="h5"
-                      display="block"
-                      width="500"
-                      style={{ fontSize: 30, textAlign: 'center' }}
-                    >
-                      Programmes
-                    </Typography>
-                  </Paper>
-                </FadeInWhenVisible>
+          <Container maxWidth="lg">
+            <FadeInWhenVisible>
+              <Paper
+                className={styles.heading}
+                style={{ background: '#012970', color: '#fff' }}
+                elevation={2}
+              >
+                <i
+                  class="fas fa-graduation-cap"
+                  style={{ margin: '0 1.2rem', padding: '0' }}
+                ></i>
+                Programs
+              </Paper>
+            </FadeInWhenVisible>
+            <Grid
+              container
+              direction="row"
+              justify="center"
+              spacing={5}
+              style={{ width: '100%', margin: 'auto' }}
+            >
+              <Grid key={Programs[0]} item xs={12} sm={6} md={6} lg={6}>
+                <ProgramCard data={bTechProgram} program={Programs[0]} />
               </Grid>
-              <Grid item xs={12}>
-                <FadeUpBigDataWhenVisible>
-                  <Paper className={classes.paper}>
-                    <Typography
-                      component="h4"
-                      style={{
-                        fontSize: 20,
-                        fontWeight: 'bold',
-                        textAlign: 'center',
-                      }}
-                    >
-                      Academic Programs
-                    </Typography>
-                    <Typography className={classes.text}>
-                      The Institute offers the following Academic Programs:
-                    </Typography>
-                    <br />
-                    <TableContainer
-                      className={classes.tablewrapper}
-                      component={Paper}
-                      elevation={0}
-                    >
-                      <Table
-                        className={classes.table}
-                        aria-label="simple table"
-                      >
-                        <TableBody>
-                          <TableRow className={classes.heads}>
-                            Bachelor of Technology Programs
-                          </TableRow>
-                          {btechrows.map((row) => (
-                            <TableRow key={row.name}>
-                              <TableCell
-                                component="th"
-                                scope="row"
-                                className={classes.text}
-                              >
-                                <Link
-                                  href={row.link}
-                                  target="_blank"
-                                  rel="noopener noreferrer"
-                                >
-                                  {row.name}
-                                </Link>
-                              </TableCell>
-                            </TableRow>
-                          ))}
-                        </TableBody>
-                        <br />
-                        <br />
-                        <TableBody>
-                          <TableRow className={classes.heads}>
-                            Master of Technology Programs
-                          </TableRow>
-                          {mtechrows.map((row) => (
-                            <TableRow key={row.name}>
-                              <TableCell
-                                component="th"
-                                scope="row"
-                                className={classes.text}
-                              >
-                                <Link
-                                  href={row.link}
-                                  target="_blank"
-                                  rel="noopener noreferrer"
-                                >
-                                  {row.name}
-                                </Link>
-                              </TableCell>
-                            </TableRow>
-                          ))}
-                        </TableBody>
-                        <br />
-                        <br />
-                        <TableBody>
-                          <TableRow className={classes.heads}>
-                            Master of Science Programs
-                          </TableRow>
-                          {mscrows.map((row) => (
-                            <TableRow key={row.name}>
-                              <TableCell
-                                component="th"
-                                scope="row"
-                                className={classes.text}
-                              >
-                                <Link
-                                  href={row.link}
-                                  target="_blank"
-                                  rel="noopener noreferrer"
-                                >
-                                  {row.name}
-                                </Link>
-                              </TableCell>
-                            </TableRow>
-                          ))}
-                        </TableBody>
-                        <br />
-                        <br />
-                        <TableBody>
-                          <TableRow className={classes.heads}>
-                            Master of Science - Master of Technology Programs
-                          </TableRow>
-                          {mscmtechrows.map((row) => (
-                            <TableRow key={row.name}>
-                              <TableCell
-                                component="th"
-                                scope="row"
-                                className={classes.text}
-                              >
-                                <Link
-                                  href={row.link}
-                                  target="_blank"
-                                  rel="noopener noreferrer"
-                                >
-                                  {row.name}
-                                </Link>
-                              </TableCell>
-                            </TableRow>
-                          ))}
-                        </TableBody>
-                        <br />
-                        <br />
-                        <TableBody>
-                          <TableRow className={classes.heads}>
-                            Master of Technology - Doctor of Philosophy
-                            (M.Tech.-Ph.D.) Dual Degree Programs
-                          </TableRow>
-                          {mtechphdrows.map((row) => (
-                            <TableRow key={row.name}>
-                              <TableCell
-                                component="th"
-                                scope="row"
-                                className={classes.text}
-                              >
-                                <Link
-                                  href={row.link}
-                                  target="_blank"
-                                  rel="noopener noreferrer"
-                                >
-                                  {row.name}
-                                </Link>
-                              </TableCell>
-                            </TableRow>
-                          ))}
-                        </TableBody>
-                        <br />
-                        <br />
-                        <TableBody>
-                          <TableRow className={classes.heads}>
-                            Doctor of Philosophy Programs
-                          </TableRow>
-                          <br />
-                          <Typography className={classes.text}>
-                            Core Sciences
-                          </Typography>
-                          {phdcorerows.map((row) => (
-                            <TableRow key={row.name}>
-                              <TableCell
-                                component="th"
-                                scope="row"
-                                className={classes.text}
-                              >
-                                <Link
-                                  href={row.link}
-                                  target="_blank"
-                                  rel="noopener noreferrer"
-                                >
-                                  {row.name}
-                                </Link>
-                              </TableCell>
-                            </TableRow>
-                          ))}
-                          <br />
-                          <Typography className={classes.text}>
-                            Engineering Sciences
-                          </Typography>
-                          {phdesrows.map((row) => (
-                            <TableRow key={row.name}>
-                              <TableCell
-                                component="th"
-                                scope="row"
-                                className={classes.text}
-                              >
-                                <Link
-                                  href={row.link}
-                                  target="_blank"
-                                  rel="noopener noreferrer"
-                                >
-                                  {row.name}
-                                </Link>
-                              </TableCell>
-                            </TableRow>
-                          ))}
-                          <br />
-                          <Typography className={classes.text}>
-                            Inter-disciplinary Areas
-                          </Typography>
-                          {phdintrows.map((row) => (
-                            <TableRow key={row.name}>
-                              <TableCell
-                                component="th"
-                                scope="row"
-                                className={classes.text}
-                              >
-                                <Link
-                                  href={row.link}
-                                  target="_blank"
-                                  rel="noopener noreferrer"
-                                >
-                                  {row.name}
-                                </Link>
-                              </TableCell>
-                            </TableRow>
-                          ))}
-                          <br />
-                          <Typography className={classes.text}>
-                            Humanities and Social Sciences
-                          </Typography>
-                          {phdhumrows.map((row) => (
-                            <TableRow key={row.name}>
-                              <TableCell
-                                component="th"
-                                scope="row"
-                                className={classes.text}
-                              >
-                                <Link
-                                  href={row.link}
-                                  target="_blank"
-                                  rel="noopener noreferrer"
-                                >
-                                  {row.name}
-                                </Link>
-                              </TableCell>
-                            </TableRow>
-                          ))}
-                        </TableBody>
-                      </Table>
-                    </TableContainer>
-                  </Paper>
-                </FadeUpBigDataWhenVisible>
+              <Grid key={Programs[1]} item xs={12} sm={6} md={6} lg={6}>
+                <ProgramCard data={mTechProgram} program={Programs[1]} />
+              </Grid>
+              <Grid key={Programs[2]} item xs={12} sm={6} md={6} lg={6}>
+                <ProgramCard data={mScProgram} program={Programs[2]} />
+              </Grid>
+              <Grid key={Programs[3]} item xs={12} sm={6} md={6} lg={6}>
+                <ProgramCard data={mScMtechProgram} program={Programs[3]} />
+              </Grid>
+              <Grid key={Programs[4]} item xs={12} sm={6} md={6} lg={6}>
+                <ProgramCard data={mTechPhdProgram} program={Programs[4]} />
+              </Grid>
+              <Grid key={Programs[5]} item xs={12} sm={6} md={6} lg={6}>
+                <ProgramCard data={phdCore} program={Programs[5]} />
+              </Grid>
+              <Grid key={Programs[6]} item xs={12} sm={6} md={6} lg={6}>
+                <ProgramCard
+                  data={phdEngineeringSciences}
+                  program={Programs[6]}
+                />
+              </Grid>
+              <Grid key={Programs[7]} item xs={12} sm={6} md={6} lg={6}>
+                <ProgramCard
+                  data={phdInterDisciplinary}
+                  program={Programs[7]}
+                />
+              </Grid>
+              <Grid key={Programs[8]} item xs={12} sm={6} md={6} lg={6}>
+                <ProgramCard data={phdHumanities} program={Programs[8]} />
               </Grid>
             </Grid>
           </Container>
