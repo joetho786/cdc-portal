6fb9eae71acd2fccf0840282a879523cd9b355b3
manav1403 <manavavni@gmail.com>
2021-07-28 22:43:08 +0530

    changed admin pannel name

--- backend/cdc_portal/urls.py
+++ backend/cdc_portal/urls.py
@@ -20,6 +20,8 @@ from django.conf.urls.static import static
 
 from.views import Login, GoogleLogin, LDAPOAuth, Config
 
+admin.site.site_header = 'CDC Admin Pannel'
+
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
