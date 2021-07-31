03c1f83be06684b8ee01d9b8685e6c8c38e7c910
Sahil1479 <santosh.2@iitj.ac.in>
2021-07-31 13:52:37 +0530

    Fixed typo, plot's v-scale and updated procedure

--- frontend/src/components/PlacementProcedure/PlacementProcedure.js
+++ frontend/src/components/PlacementProcedure/PlacementProcedure.js
@@ -8,10 +8,8 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import { Container } from '@material-ui/core';
 import StarsIcon from '@material-ui/icons/Stars';
-import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import FadeInWhenVisible from '../Animation/FadeIn';
-import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -83,16 +81,8 @@ const PlacementProcedureComponent = () => {
               icon={1}
             >
               <p className={classes.card}>
-                CDC sends invitation to companies and organizations. companies
-                can also send us a mail at{' '}
-                <a
-                  href={
-                    'https://mail.google.com/mail/?view=cm&fs=1&to=placement@iitj.ac.in'
-                  }
-                >
-                  placement@iitj.ac.in
-                </a>{' '}
-                for same.
+                Career Development Cell reaches out to various prestigious firms
+                notifying that IIT Jodhpur is open for hiring.
               </p>
             </VerticalTimelineElement>
             <VerticalTimelineElement
@@ -101,19 +91,9 @@ const PlacementProcedureComponent = () => {
               icon={2}
             >
               <p className={classes.card}>
-                Companies participating in Annual Placement Meet at IIT Jodhpur
-                will be required to register at the Career Development Cell
-                (CDC) website
-                <span> </span>
-                <a href="/">(https://osp.iitj.ac.in/)</a>.
+                Companies interested reach out CDC with the details CDC needs
+                about the roles extended by the company.
               </p>
-              <div style={{ marginTop: 10, marginRight: 5 }}>
-                <Link to="/recruiter-login" style={{ textDecoration: 'none' }}>
-                  <Button variant="outlined" size="medium" color="primary">
-                    Register Now
-                  </Button>
-                </Link>
-              </div>
             </VerticalTimelineElement>
             <VerticalTimelineElement
               className="vertical-timeline-element--work"
@@ -121,34 +101,10 @@ const PlacementProcedureComponent = () => {
               icon={3}
             >
               <p className={classes.card}>
-                Fill out the JAF (Job Announcement Form)/ IAF (Intern
-                Announcement Form) and send a soft copy of the same to the
-                Placement Team. The CDC will verify the information provided by
-                the company, following which a login ID and a password for the
-                online CDC Portal will be provided to the companies.
+                Details of eligible candidates who are interested for the role
+                will be made available to interested companies. Company can
+                provide insight to students with their pre-placement talks.
               </p>
-              <div style={{ marginTop: 10 }}>
-                <Link to="/jaf" style={{ textDecoration: 'none' }}>
-                  <Button
-                    variant="outlined"
-                    size="medium"
-                    color="primary"
-                    style={{ marginInlineEnd: 10 }}
-                  >
-                    JAF
-                  </Button>
-                </Link>
-                <Link to="/iaf" style={{ textDecoration: 'none' }}>
-                  <Button
-                    variant="outlined"
-                    size="medium"
-                    color="primary"
-                    style={{ marginInlineEnd: 10 }}
-                  >
-                    IAF
-                  </Button>
-                </Link>
-              </div>
             </VerticalTimelineElement>
             <VerticalTimelineElement
               className="vertical-timeline-element--education"
@@ -156,8 +112,9 @@ const PlacementProcedureComponent = () => {
               icon={4}
             >
               <p className={classes.card}>
-                The dates of the Pre Placement Talks (PPT) will be decided with
-                the mutual consent of CDC and the Organisation.
+                Students go through screening tests mandated by the company.
+                Shortlisted students move forward in the company’s selection
+                process.
               </p>
             </VerticalTimelineElement>
             <VerticalTimelineElement
@@ -166,8 +123,8 @@ const PlacementProcedureComponent = () => {
               icon={5}
             >
               <p className={classes.card}>
-                Based on the PPT and profile of the job, students will be asked
-                to apply for the job.
+                CDC notifies the companies with the feasible dates and schedules
+                hiring process after finalising the dates.
               </p>
             </VerticalTimelineElement>
             <VerticalTimelineElement
@@ -176,22 +133,9 @@ const PlacementProcedureComponent = () => {
               icon={6}
             >
               <p className={classes.card}>
-                Placement Session starts in the month of October. Early
-                recruitment offers will be entertained based on the students'
-                response for the same. A suitable date and time slot will be
-                provided to the company based on criteria like: Student
-                Preference, The Job Profile, Pay Package offered, The growth
-                prospects.
-              </p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-              className="vertical-timeline-element--education"
-              iconStyle={IconStyle}
-              icon={7}
-            >
-              <p className={classes.card}>
-                The company/organisation is required to furnish the results by
-                the end of the day of recruitment.
+                Companies provides the final list of the selected students. CDC
+                ensuring that all ends are met, will request for feedback to
+                improvise the expereince.
               </p>
             </VerticalTimelineElement>
             <VerticalTimelineElement
