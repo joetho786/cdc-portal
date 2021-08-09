c530d8488061145020502e9c91e57d759de055a0
DarshitJain04 <jain.38@iitj.ac.in>
2021-08-09 22:49:44 +0530

    changed resume rename function to use roll number instead of username

--- backend/student/models.py
+++ backend/student/models.py
@@ -101,11 +101,11 @@ pre_save.connect(event_pre_save_receiver_student, sender=StudentProfile)
 def event_pre_save_receiver_resume(sender, instance, *args, **kwargs):
     if instance.student.user.first_name not in instance.file.name or \
             instance.student.user.last_name not in instance.file.name or \
-            instance.student.user.username not in instance.file.name or \
+            instance.student.roll_no not in instance.file.name or \
             'IITJodhpur.pdf' not in instance.file.name \
             and instance._state.adding is True:
         instance.file.name = instance.student.user.first_name + '_' + instance.student.user.last_name \
-            + '_' + instance.student.user.username + '_' + str(random.randint(1, 10001)) + \
+            + '_' + instance.student.roll_no + '_' + str(random.randint(1, 10001)) + \
             '_' + 'IITJodhpur.pdf'
     if not instance.reference:
         instance.reference = instance.file.name
