ef26cf7f19912d94ee729c5dd2f3bb053093ceb8
unknown <santosh.2@iitj.ac.in>
2021-10-19 15:20:19 +0530

    Added check for banned profile

--- backend/student/views.py
+++ backend/student/views.py
@@ -161,15 +161,16 @@ class AvailableOffers(APIView):
             id__in=m.objects.filter(student__user=user).values_list('profile'))
 
     def get_offers(self, profile, model):
-        if profile.program_branch.check_gpa:
-            offers = model.objects.filter(min_gpa__lte=profile.gpa, min_ug_gpa__lte=profile.ug_gpa,
-                                          eligible_program_branch__getter__contains=profile.program_branch.getter,
-                                          active=True).difference(self.get_applied_ad_list(profile.user, model))
-        else:
-            offers = model.objects.filter(min_gpa__lte=profile.gpa,
-                                          eligible_program_branch__getter__contains=profile.program_branch.getter,
-                                          active=True).difference(self.get_applied_ad_list(profile.user, model))
-        return offers
+        if(profile.banned==False):
+            if profile.program_branch.check_gpa:
+                offers = model.objects.filter(min_gpa__lte=profile.gpa, min_ug_gpa__lte=profile.ug_gpa,
+                                            eligible_program_branch__getter__contains=profile.program_branch.getter,
+                                            active=True).difference(self.get_applied_ad_list(profile.user, model))
+            else:
+                offers = model.objects.filter(min_gpa__lte=profile.gpa,
+                                            eligible_program_branch__getter__contains=profile.program_branch.getter,
+                                            active=True).difference(self.get_applied_ad_list(profile.user, model))
+            return offers
 
     def get(self, request):
         show = get_config_value('ShowAdertisements')
